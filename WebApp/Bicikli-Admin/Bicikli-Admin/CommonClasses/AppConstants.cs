737294bf7356606f45167940dd1c13903a3f4f11
takraj <takraj@gmail.com>
2012-11-01 20:53:24 +0100

    Néhány helper cucc... + 8080-as port fixen
    Signed-off-by: takraj <takraj@gmail.com>

---
+++ WebApp/Bicikli-Admin/Bicikli-Admin/CommonClasses/AppConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class AppConstants
+{
+    public const string AppName = "Bicikli-Admin v1.0";
+    public const string ErrorReportEmail = "support@example.com";
+}
